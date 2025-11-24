94f483c62a6ca0b7ac23761b589b1d257537b5c1
Noynto <support@noynto.me>
2025-11-24 22:07:28 +0100

    initialisation of projet with Gradle

---
+++ bootstrap/src/main/java/me/noynto/fortress/finance/Bootstrap.java
@@ -0,0 +1,8 @@
+package me.noynto.fortress.finance;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        System.out.println("Bootstrapping application");
+    }
+}
---
+++ bootstrap/src/test/java/me/noynto/fortress/finance/BootstrapTest.java
@@ -0,0 +1,15 @@
+package me.noynto.fortress.finance;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BootstrapTest {
+
+    @Test
+    void appHasAGreeting() {
+        Bootstrap.main(null);
+    }
+
+}
